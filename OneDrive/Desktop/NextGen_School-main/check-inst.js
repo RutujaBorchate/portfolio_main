b74fcd09840e180b3171297e4baad1726dda297d
RutujaBorchate <borchaterutuja496@gmail.com>
2026-04-16 10:06:33 +0530

    Delete OneDrive/Desktop/NextGen_School-main directory

--- OneDrive/Desktop/NextGen_School-main/check-inst.js
+++
@@ -1,4 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('app.db');
-const rows = db.prepare("SELECT * FROM users WHERE role = 'institution'").all();
-console.log(JSON.stringify(rows, null, 2));
--- OneDrive/Desktop/NextGen_School-main/check.js
+++
@@ -1,5 +0,0 @@
-const Database = require('better-sqlite3');
-const fs = require('fs');
-const db = new Database('app.db');
-const tables = db.prepare('PRAGMA table_info(users)').all();
-fs.writeFileSync('output.json', JSON.stringify(tables, null, 2), 'utf8');
